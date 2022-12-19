9ffb8dc3940a736c3848de88c6851dc98ddf7c04
Harish <harishseervi07@gmail.com>
2022-12-19 17:53:15 +0530

    added Scroll Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
-const toggleBackground = function(){
-    const getClass = document.getElementsByTagName("BODY")[0].classList ; //Grabbing the body's classlist
+const toggleBackground = function () {
+    const getClass = document.getElementsByTagName("BODY")[0].classList; //Grabbing the body's classlist
     const nav = document.getElementsByClassName("navbar")[0].classList;
 
-    for(let i = 0; i < 2; i++){
-    const darkButton = document.querySelectorAll(".iconDark")[i].classList;
-    const lightButton = document.querySelectorAll(".iconLight")[i].classList;
-    darkButton.toggle("iconToggle");
-    lightButton.toggle("iconToggle2");
+    for (let i = 0; i < 2; i++) {
+        const darkButton = document.querySelectorAll(".iconDark")[i].classList;
+        const lightButton = document.querySelectorAll(".iconLight")[i].classList;
+        darkButton.toggle("iconToggle");
+        lightButton.toggle("iconToggle2");
     }
 
     getClass.toggle("toggleClass");
     nav.toggle("toggleClass");
-   
+
     for (let i = 0; i < 5; i++) {
         const element = document.getElementsByClassName("navigationLink")[i].classList;
         element.toggle("toggleClass");
     }
 
     for (let i = 0; i < 6; i++) {
-    const socialDarkButton = document.querySelectorAll(".socialDark")[i].classList; 
-    const socialLightButton = document.querySelectorAll(".socialLight")[i].classList;
-        
-     console.log(socialDarkButton);
-    socialDarkButton.toggle("iconToggle");
-    socialLightButton.toggle("iconToggle2");
-        
-    } 
+        const socialDarkButton = document.querySelectorAll(".socialDark")[i].classList;
+        const socialLightButton = document.querySelectorAll(".socialLight")[i].classList;
+
+        console.log(socialDarkButton);
+        socialDarkButton.toggle("iconToggle");
+        socialLightButton.toggle("iconToggle2");
+
+    }
 
 }
 
@@ -35,41 +35,65 @@ let skillCategoryName = "";
 //One by one sare skill card pe event listener lagae and remember event object sirf event listener ke through milta h
 for (let i = 0; i < 3; i++) {
     const element = document.getElementsByClassName("skills_content")[i];
-    element.addEventListener("click",function(event){
+    element.addEventListener("click", function (event) {
         skillCategoryName = event.target.innerText;
         if (skillCategoryName === "FrontEnd Development") {
             const element = document.getElementsByClassName("uil-angle-down")[0];
             element.classList.toggle("skills_animation_down");
             const skillCard = document.getElementsByClassName("skills_hide")[0].classList;
             skillCard.toggle("skills_show");
-        } 
-        else if(skillCategoryName === "BackEnd Development"){
+        }
+        else if (skillCategoryName === "BackEnd Development") {
             const element = document.getElementsByClassName("uil-angle-down")[1];
             element.classList.toggle("skills_animation_down");
             const skillCard = document.getElementsByClassName("skills_hide")[1].classList;
             skillCard.toggle("skills_show");
         }
-        else if(skillCategoryName === "Some other Tools"){
+        else if (skillCategoryName === "Some other Tools") {
             const element = document.getElementsByClassName("uil-angle-down")[2];
             element.classList.toggle("skills_animation_down");
             const skillCard = document.getElementsByClassName("skills_hide")[2].classList;
             skillCard.toggle("skills_show");
         }
     })
-    
+
 }
 
 
 
 const menu = document.getElementById("hamburgerMenu");
-menu.addEventListener("click",function(){  
-    const nav = document.getElementsByClassName("navbar")[0].classList; 
-    const menu = document.getElementsByClassName("menuContainer")[0]; 
+menu.addEventListener("click", function () {
+    const nav = document.getElementsByClassName("navbar")[0].classList;
+    const menu = document.getElementsByClassName("menuContainer")[0];
     menu.style.flexDirection = "column";
     nav.toggle("hamburgerNav");
-       for(let i = 0; i < 5; i++){
-           const menuItem = document.getElementsByTagName("li")[i].classList;
-           menuItem.toggle("showMenu");
-       }
+    for (let i = 0; i < 5; i++) {
+        const menuItem = document.getElementsByTagName("li")[i].classList;
+        menuItem.toggle("showMenu");
+    }
 })
 console.log(menu);
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showObserver');
+        }
+        else {
+            entry.target.classList.remove('showObserver');
+
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.observeElement');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const scrollDown = document.querySelector(".scroll-down")
+scrollDown.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.screenY(200)
+})
